a8e5847587ae75d53b6d59acdc54c78d3892a872
Tristan <tristancosta0307@gmail.com>
2020-08-01 19:50:19 -0300

    .

--- src/Components/Field/index.js
+++ src/Components/Field/index.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Field ({value, name, type, label, onChange}) {
-    return (
-        <div>
-            <label>
-                {label}:
-            <input 
-                type={type}
-                name={name}
-                value={value}
-                onChange={onChange}
-            />
-            </label>
-        </div>  
-    )
+function Field({
+  value, name, type, label, onChange,
+}) {
+  const filedId = `id_${name}`;
+  return (
+    <div>
+      <label 
+        htmlFor={filedId}
+      >
+        {label}
+        :
+        <input
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
 }
+Field.defaultProps = {
+  type: 'text',
+  value: 'text',
+  onChange: () => {},
+};
 
+Field.propTypes = {
+  value: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+};
 export default Field;
--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,70 +1,81 @@
-import React, {useState} from 'react';
-import MasterPage from '../../../Components/MasterPage';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import MasterPage from '../../../Components/MasterPage';
 import Field from '../../../Components/Field';
+import Button from '../../../Components/Button';
 
-function CadastroCategoria(){
+function CadastroCategoria() {
+  const valoresIniciais = {
+    titulo: '',
+    descricao: '',
+    cor: '#000',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
-    const valoresIniciais = {
-        nome: '',
-        descricao: '',
-        cor: '#000'
-    }
-    const [categorias, setCategorias] = useState([]);
-    const [values, setValues] = useState(valoresIniciais);
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
 
-    function setValue(chave, valor){
-        setValues({
-            ...values,
-            [chave]: valor
-        })
-    }
+  function handleSubmit(args) {
+    args.preventDefault();
+    setCategorias([
+      ...categorias, values,
+    ]);
+    setValues(valoresIniciais);
+  }
 
-    function handleSubmit(args){
-        args.preventDefault();
-        setCategorias([
-            ...categorias, values
-        ]);
-        setValues(valoresIniciais);
-    }
+  function handleChange(args) {
+    const { value } = args.target;
 
-    function handleChange(args){
-        const { getAttribute, value } = args.target;
-        
-        setValue(
-            args.target.name,
-            value
-            );
-    }
+    setValue(
+      args.target.name,
+      value,
+    );
+  }
 
-    return (
-        <MasterPage>
-            <h1>Cadstro de categoria: {values.nome}</h1>
+  useEffect(() => {
+    const URL = ' http://localhost:8080/categorias';
+    fetch(URL).then(async (respsotaDoServidor) => {
+      const resposta = await respsotaDoServidor.json();
+      setCategorias([
+        ...resposta,
+      ]);
+    });
+  }, []);
 
-            <form onSubmit={handleSubmit}>
-                <Field name={'nome'} type={"text"} label={"Nome"} value={values.nome} onChange={handleChange} />
-                <Field name={'descricao'} type={"text"} label={"Descrição"} value={values.descricao} onChange={(handleChange)} />
-                <Field name={'cor'} type={"color"} label={"Cor"} value={values.cor} onChange={handleChange} />
+  return (
+    <MasterPage>
+      <h1>
+        Cadstro de categoria:
+        {values.titulo}
+      </h1>
 
-                <button>
-                    Cadastrar
-                </button>
-            </form>
+      <form onSubmit={handleSubmit}>
+        <Field name="nome" type="text" label="Nome" value={values.titulo} onChange={handleChange} />
+        <Field name="descricao" type="text" label="Descrição" value={values.descricao} onChange={(handleChange)} />
+        <Field name="cor" type="color" label="Cor" value={values.cor} onChange={handleChange} />
 
-                <ul>
-                    {categorias.map((categoria, index) => {
-                        return (
-                            <li key={`${categoria}${index}`}>
-                                {categoria.nome}
-                            </li>
-                        )
-                    })}
-                </ul>
-            <Link to="/">
-                Ir para a Home
-            </Link>
-        </MasterPage>
-    )
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria}`}>
+            {categoria.titulo}
+          </li>
+        ))}
+      </ul>
+      <Link to="/">
+        Ir para a Home
+      </Link>
+    </MasterPage>
+  );
 }
 
 export default CadastroCategoria;
